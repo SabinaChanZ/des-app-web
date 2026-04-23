4a7fd5722b5a1d2cd490cbc666d8360e31ea1b1d
SabinaChanZ <240171@mail.unacar.mx>
2026-04-23 12:43:02 -0600

    Subida inicial de todos los archivos del proyecto

---
+++ 04-JavaScript/main.js
@@ -0,0 +1,18 @@
+function calcularIMC(peso, altura) {
+    return peso / (altura * altura)
+}
+
+function preguntarIMC(){
+    const peso = parseFloat(prompt("¿Cuál es tu peso?"))
+    const altura = parseFloat(prompt("Cuál es tu altura?"))
+    const IMC = calcularIMC(peso, altura)
+
+    var categoria = ""
+    if (IMC < 18.5) categoria = "Bajo peso"
+    else if (IMC < 25) categoria = "Peso normal"
+    else if (IMC < 30) categoria = "Sobrepeso"
+    else categoria = "Obesidad"
+    alert("tu Indice de Masa Corporal es " + IMC + " estas con " + categoria)
+}
+
+preguntarIMC()
